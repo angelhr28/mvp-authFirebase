5ec08d4c5f67cf38428e5185550e760371fe33a5
angel <angelhuamannahui@gmail.com>
2020-10-06 22:42:40 -0500

    mvp firebase + foto

---
+++ app/src/main/java/com/example/platform_univ/modulo/login/mvp/LoginMVP.kt
@@ -0,0 +1,23 @@
+package com.example.platform_univ.modulo.login.mvp
+
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+
+interface LoginMVP {
+    interface View{
+        fun showProgres()
+        fun hideProgres()
+        fun showToask(message:String)
+        fun logInSuccess()
+        fun logInError()
+        fun fuegosAtifi()
+    }
+
+    interface Presenter{
+        fun logIn(user:String, password:String)
+    }
+
+    interface Model{
+        fun logIn(user:String, password:String): Task<AuthResult>
+    }
+}
